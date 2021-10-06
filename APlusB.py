3f6f4f6f9cf47a275513e3eddd9476bff3deaf8e
Konstantin Shitkov <52782381+shitkov@users.noreply.github.com>
2021-10-06 16:46:27 +0300

    Created using Colaboratory

---
+++ APlusB.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "alg_w1_2021-10-06_v0.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyPh3ctwhx+FXlmNqVC6M3ig",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/shitkov/bert4classification/blob/main/APlusB.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "1mszBwdS01Ph"
+      },
+      "source": [
+        "def sum_of_two_digits(first_digit, second_digit):\n",
+        "    return first_digit + second_digit\n",
+        "\n",
+        "if __name__ == '__main__':\n",
+        "    a, b = map(int, input().split())\n",
+        "    print(sum_of_two_digits(a, b))"
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
